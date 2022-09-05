de1277bb227e43f673d67e315f1ee61be6fa5a6c
Paulo-vca <paulovca26@gmail.com>
2022-09-04 22:59:07 -0400

    ajustando

--- ProjetoPoo/src/lp/ex_robo/Mover.java
+++ ProjetoPoo/src/lp/ex_robo/Mover.java
@@ -1,7 +1,80 @@
 package ProjetoPoo.src.lp.ex_robo;
 
+import java.io.IOException;
+import java.util.Scanner;
+
+
+
 public class Mover {
-    public static void main(String[] args) {
+    private final static String[] opcoes = {
+        "\n\n",
+        " -----------------------------------------------------------------",
+        "|               +++ Sistema para mover o Robo +++                 |",
+        "|-----------------------------------------------------------------|",
+        "|                      Escolha uma opção                          |",
+        "|-----------------------------------------------------------------|",
+        "|                                                                 |",
+        "| 1 - Mover-se para cima                                          |",
+        "| 2 - Mover-se para direita                                       |",
+        "| 3 - Mover-se para baixo                                         |",
+        "| 4 - Mover-se para esquerda                                      |",
+        "| 0 - Sair                                                        |",
+        "|                                                                 |",
+        " -----------------------------------------------------------------",
         
+};
+    public static void main(String[] args) throws IOException{
+        Scanner in = new Scanner(System.in);
+        int x;
+        int y;
+        int adicionar = 0;
+
+        System.out.print("Digite a posição inicial x: ");
+            x = in.nextInt();
+        System.out.print("\nDigite a posição inicial y: ");
+            y = in.nextInt();
+
+        Robo mover = new Robo(x,y);
+
+        private static void buffer = new Scanner(System.in);
+
+
+        int op = 1;
+        while (op != 0) {
+            printMenu();
+            System.out.print("Digite Aqui: ");
+            op = buffer.nextInt();
+            escolha(op);
+            System.in.read();
+        }
     }
-}
+        private static void printMenu() {
+            System.out.print("\033[H\033[2j");
+            System.out.flush();
+            for (String op : opcoes) {
+                System.out.println(op);
+            }
+        }
+
+        private static void escolha(int op) {
+            switch (op) {
+                case 1:
+                Robo.top(adicionar);
+                break;
+                case 1:
+                Robo.top(adicionar);
+                break;
+                case 1:
+                Robo.top(adicionar);
+                break;
+                case 1:
+                Robo.top(adicionar);
+                break;
+                case 1:
+                Robo.top(adicionar);
+                break;
+            }
+        }
+
+    }
+
--- ProjetoPoo/src/lp/ex_robo/Robo.java
+++ ProjetoPoo/src/lp/ex_robo/Robo.java
@@ -3,15 +3,15 @@ package ProjetoPoo.src.lp.ex_robo;
 
 
 public class Robo {
-    private double x;
-    private double y;
+    private int x;
+    private int y;
 
  //GET AND SET
     public double getX() {
         return this.x;
     }
 
-    public void setX(double x) {
+    public void setX(int x) {
         this.x = x;
     }
 
@@ -19,30 +19,30 @@ public class Robo {
         return this.y;
     }
 
-    public void setY(double y) {
+    public void setY(int y) {
         this.y = y;
     }
 
-    public Robo(double _x, double _y) { 
+    public Robo(int _x, int _y) { 
         this.x = _x;
         this.y = _y;
     }
     
 
     public void move(char dir, int qtoMove) {}
-
+    //cima
     public void top(int adicionar){
         y += adicionar;
     }
- 
+    //baixo
     public void down(int diminuir){
         y -= diminuir;
     }
-
+    //esquerda
     public void left(int adicionar){
         x += adicionar;
     }
-
+    //direita
     public void right(int diminuir){
         x -= diminuir;
     }
