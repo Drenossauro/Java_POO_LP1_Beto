6be1472e3a57a0cd65d91f1cf37662d62511953e
Paulo_vca <paulovca26@gmail.com>
2022-10-17 22:04:45 -0400

    17/10

--- ProjetoPoo/src/lp/lista/ListaLinear.java
+++ ProjetoPoo/src/lp/lista/ListaLinear.java
@@ -1,16 +1,20 @@
 package ProjetoPoo.src.lp.lista;
 
+import java.util.List;
+
 public class ListaLinear {
 
     public static class Lista {
         public No inicio;// primeiro elementoda lista
         public int tam;//tamanho da lista
+        public No fim;
+        public No fimv2;
     }
 
     // inner class
     public static class No {
         public int info;
-        public No prox;
+        public No prox;//armazenando o proximo endereço do elemento
     }
 
     // ----------------------------------------------------------------------------------------------------------------------
@@ -34,17 +38,34 @@ public class ListaLinear {
         l.tam = 0;
         return l;
     }
+
     // --------------------------------------------------------------------------------------------------------------------------
 
     public static Lista addInicio(Lista l, int val) {
 
         No n1 = new No();
         n1.info = val;
+
+        if(isEmpty(l)){
+            n1.prox = l.inicio;
+            l.inicio = n1;
+            l.fim = n1;
+            return l;
+        }
+
         n1.prox = l.inicio;
         l.inicio = n1;
         l.tam++;
         return l;
     }
+
+    // --------------------------------------------------------------------------------------------------------------------------
+
+    public static boolean isEmpty(Lista l) {
+        return l.tam == 0 ? true : false;
+        //return l.inicio == 0;
+    }
+
 
     // --------------------------------------------------------------------------------------------------------------------------
 
@@ -78,6 +99,21 @@ public class ListaLinear {
         return l;
 
     }
+    // ---------------------------------------------------------------------------------------------------------
+
+    public static Lista addFimv2(Lista l, int val) {
+
+        if (isEmpty(l)){
+            l = addInicio(l, val);//Leticia e Andre
+            return l;
+        }
+        No novo = new No();
+        novo.info = val;
+        l.fim.prox = novo;
+        l.fim = novo;  
+        return l;      
+    }
+
 
     // ---------------------------------------------------------------------------------------------------------
     public static void printLista(Lista l) {
