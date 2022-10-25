e2696ca8d9b4dcae56ea44ca6b0e240f5754156a
Paulo_vca <paulovca26@gmail.com>
2022-10-24 21:26:06 -0400

    obs

--- ProjetoPoo/src/revisao/Colecao.java
+++ ProjetoPoo/src/revisao/Colecao.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Colecao {
- 
+
     public static void main(String[] args) {
 
         List<Autor> lista = new ArrayList<Autor>();
